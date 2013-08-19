409c7877df2beaf79e61a19034915eeb5eded1f4
Sean McCracken <seanmccracken13@gmail.com>
2013-08-19 10:17:59 -0600

    Stubbing out Project and created EnemyBase class
    starting.

---
+++ SuperPaperMonsterSmasher/Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EnemyType{ basic, shield, fast };
+
+public class EnemyBase 
+{
+	private int _health = 0;
+	private float _speed = 0.0f;
+	private int _id = 0;
+	private EnemyType _enemyType;
+	
+	
+
+	public EnemyBase()
+	{
+		//Constructor	
+	}
+	
+	public virtual void SetHealth()
+	{
+			
+	}
+	
+	public virtual void SetSpeed()
+	{
+		
+	}
+	
+	public virtual void SetID()
+	{
+		
+	}
+	
+	public virtual void SetEnemyType()
+	{
+		
+	}
+	
+	public virtual int GetHealth()
+	{
+		return _health;	
+	}
+	
+	public virtual float GetSpeed()
+	{
+		return _speed;	
+	}
+	
+	public virtual int GetID()
+	{
+		return _id;
+	}
+	
+	public virtual EnemyType GetEnemyType()
+	{
+		return _enemyType;	
+	}
+	
+	
+}
