46006f25183f573a965ebb5d292bbff94e2bc303
Sean McCracken <seanmccracken13@gmail.com>
2013-08-22 12:47:03 -0600

    StartMenuScene is working without Music yet.
    Open Scenes > StartMenuScene to see what I did.

---
+++ SuperPaperMonsterSmasher/Assets/Scripts/ViewControllers/StartScreen/StartScreenController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenController : MonoBehaviour {
+
+	bool _isMobile = false;
+	bool _startNextLevelLock = false;
+	
+	void Start () 
+	{
+		if(!Application.isEditor && !Application.isWebPlayer)
+		{
+			_isMobile = true;	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(_isMobile)
+		{
+			MobileUpdate();
+		} else {
+			EditorUpdate();
+		}
+	}
+	
+	void MobileUpdate()
+	{
+		if(Input.touchCount > 0 && !_startNextLevelLock)
+		{
+			
+			LockAndLoadGame();
+			
+		}
+	}
+	
+	void EditorUpdate()
+	{
+		if(Input.GetMouseButtonDown(0) && !_startNextLevelLock)
+		{
+			
+			LockAndLoadGame();
+			
+		}
+	}
+	
+	void LockAndLoadGame()
+	{
+		_startNextLevelLock = true;	
+		
+		//Look at File > Build Settings > Scenes In Build to make sure this is working
+		
+		Application.LoadLevel("GameLevel");	
+	}
+}
